e215ed05c162f36a3f71242534ac74cb475f825b
Emrullah87 <emrullah.altuntas87@gmail.com>
2025-08-11 23:11:33 +0200

    explicit-intent: von activity zu activity wechseln

---
+++ app/src/main/java/com/amu/myintents/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.amu.myintents
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Explicit Intent to SecondActivity
+        val explicitButton = findViewById<Button>(R.id.explicitButton) //init Button
+
+        explicitButton.setOnClickListener {
+            val explicitIntent = Intent(this, SecondActivit::class.java)
+            startActivity(explicitIntent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/amu/myintents/SecondActivit.kt
@@ -0,0 +1,14 @@
+package com.amu.myintents
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
